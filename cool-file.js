cbf0fc4db78fbe6c1e91c11f1f9d569589a1f7c4
Glitch (qatarrail) <none>
2019-04-12 00:52:48 +0000

    🏹🌖 Updated with Glitch

---
+++ cool-file.js
@@ -0,0 +1,30 @@
+<table>
+   <th>
+     <body>
+      
+      Qatar is a growing country, and to accommodate this growth, a new, sustainable, and efficient way of getting both people and freight around the country was needed. Established by Emiri decree in 2011, Qatar Rail was given the mandate to design and develop the country’s rail network, and after the rail projects are completed, manage, operate, and maintain them.
+
+Qatar Rail’s answer to Qatar’s transportation challenges lies in three major projects:
+<ul>
+  • The Doha Metro: a mostly underground rail network which connects communities within Doha and its suburbs<br>
+
+• The Lusail Tram (LT): a tram network providing comfortable and convenient travel within the new city of Lusail<br>
+
+• The Long Distance Rail: connecting cities in the north and west with Doha, and the country with the forthcoming GCC rail system<br>
+</ul>
+Once all the projects are completed by 2030, the three networks will act as one integrated system, allowing passengers to easily transfer between them.
+     </body>
+         </th>
+<th><body1>
+تسعى دولة قطر إلى تحقيق التنمية الشاملة وهو ما يتطلب في جزء رئيسي منه توفير وضمان وسيلة نقل جديدة ومستدامة وفعالة للأفراد والبضائع في جميع أنحاء البلاد. وفي ضوء ذلك، تأسست شركة سكك الحديد القطرية (الريل) في عام 2011 بموجب قرار أميري نصّ على أن تتولى الشركة مسؤولية تصميم وتطوير شبكة السكك الحديدية في البلاد ثم إدارتها وتشغيلها وصيانتها فور انجازها.
+
+:وتضع شركة "الريل" حلولاً ناجحة لتحديات النقل في قطر من خلال مشاريعها العملاقة الثلاثة، وهي
+<ul>
+مترو الدوحة: شبكة سكك حديدية متطورة يمتد معظمها تحت الأرض لربط المجتمعات داخل العاصمة الدوحة وضواحيها•<br>
+
+ترام لوسيل: شبكة ترام توفر وسيلة نقل مريحة وملائمة داخل مدينة لوسيل الجديدة•<br>
+
+سكك حديد المسافات الطويلة:  يربط مدن الشمال والغرب بالدوحة، وسيكون جزءاً من شبكة السكك الحديدية المخطط لها في دول مجلس التعاون لدول الخليج العربية•<br>
+</ul>
+وبمجرد الانتهاء من جميع المشاريع بحلول عام 2030، ستشكل هذه الشبكات الثلاث منظومة نقل واحدة ومتكاملة تتيح للركاب التنقل بسهولة في ما بينها.</body1></th>
+</table>
